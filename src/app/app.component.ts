41d31fafe1d7ea6a08971e24ff374a0b783de06f
Maria Shahrai <Maria.Shahrai@mef.dev>
2023-06-21 21:55:38 +0300

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:  'plugin-example', // 'app-root',
+  selector:  'clothes-shop', // 'app-root',
   template:  '<router-outlet></router-outlet>', // <-- також замінюємо стандартний вміст
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'clothes-shop';
