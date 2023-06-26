1cd78c542a45e8b84f870a7a16bf7c140e3b7983
Панов Даниил <113932048+Danik1703@users.noreply.github.com>
2023-06-26 10:32:57 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { PlatformHelper } from  '@natec/mef-dev-platform-connector';
 import { HomePageComponent } from './home-page/home-page.component';
 //const routes: Routes = [];
 const  routes: Routes = PlatformHelper.updatePluginsRoutes([
-  {
-      path:"",
+path:"",
       children:[
+          {
+              path:"test",
+              redirectTo:"test",
+              pathMatch:"full",
+          },
           {
               path:"test",
               component:  HomePageComponent
           }
       ]
-  }
-]);
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
